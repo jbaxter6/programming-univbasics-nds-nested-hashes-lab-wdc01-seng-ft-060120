1a48784bf15a0aa6f949993c5f6b312b21a712c8
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-15 04:20:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,7 +10,14 @@ def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
   
-  aoh = [{}]
+  aoh = []
+  
+  aoh << DON_G
+  aoh << JOELLE_VD
+  aoh << PAT_M
+  aoh << BRUCE_G
+  
+  aoh
   
 end
 
